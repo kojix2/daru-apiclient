6efc68b5a36e1dea258ffeed3e7a5708785ed746
kojix2 <2xijok@gmail.com>
2019-02-02 20:09:50 +0900

    Update apiclient.rb

--- lib/daru/apiclient.rb
+++ lib/daru/apiclient.rb
@@ -6,18 +6,33 @@ module Daru
   class APIClient
     include HTTParty
 
+    attr_accessor :uri
     def initialize(uri)
-      self.class.base_uri uri
+      @uri = uri
     end
 
     def get(*args)
+      self.class.base_uri uri
       response = self.class.get(*args)
       Daru::DataFrame.new(response.to_a)
     end
 
     def post(*args)
+      self.class.base_uri uri
       response = self.class.post(*args)
       Daru::DataFrame.new(response.to_a)
+    end
+    
+    def hget
+      self.class.base_uri uri
+      response = self.class.get(*args)
+      response.to_h
+    end
+    
+    def hpost
+      self.class.base_uri uri
+      response = self.class.post(*args)
+      response.to_h
     end
   end
 end
