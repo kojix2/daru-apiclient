273f2cbcdb984d7f14d5834e7ca8cee0278a0882
kojix2 <2xijok@gmail.com>
2019-02-03 12:26:29 +0900

    support multiple clients

--- lib/daru/apiclient.rb
+++ lib/daru/apiclient.rb
@@ -1,23 +1,47 @@
 require 'httparty'
 require 'daru'
-require "daru/apiclient/version"
+require 'daru/apiclient/version'
 
 module Daru
-  class APIClient
+  class APIClientTemplate
     include HTTParty
-
-    def initialize(uri)
-      self.class.base_uri uri
-    end
 
     def get(*args)
       response = self.class.get(*args)
       Daru::DataFrame.new(response.to_a)
-    end
+  end
 
     def post(*args)
       response = self.class.post(*args)
       Daru::DataFrame.new(response.to_a)
+    end
+  end
+
+  class APIClient
+    def initialize(uri)
+      client_class = Class.new(Daru::APIClientTemplate)
+      client_class.base_uri uri
+      @c = client_class.new
+    end
+
+    def get(*args)
+      @c.get(*args)
+    end
+
+    def post(*args)
+      @c.post(*args)
+    end
+
+    def hget(*args)
+      @c.class.get(*args).to_h
+    end
+
+    def hpost(*args)
+      @c.class.post(*args).to_h
+    end
+
+    def c
+      @c.class
     end
   end
 end
